d3196d9613207b64625c51daef9bad68b47107ef
pavan042533 <chavapavankumar1234@gmail.com>
2025-12-16 14:11:38 +0530

    feat: Refactor medication route handlers to use newMedicationHandler

--- public/init.go
+++ public/init.go
@@ -1,15 +1,26 @@
 package public
 
 import (
-	"ai_health_assistant/public/medication"
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"ai_health_assistant/pkg/medication"
 )
 
 func MountRoutes(app *fiber.App) {
 	api:= app.Group("/api")
+
+	medicationService := medication.NewMedicationService()
+	medicationHandler:= newMedicationHandler(medicationService)
+
 	medicationGroup := api.Group("/medication")
 	fmt.Println("Mounting medication routes")
-	medicationGroup.Post("scan-prescription", medication.HandleScanPrescription)
+	
+	medicationGroup.Post("scan-prescription", medicationHandler.HandleScanPrescription)
+	medicationGroup.Post("review-medicine", medicationHandler.HandleReviewMedications)
+	// medicationGroup.Post("save", medication.HandleSaveMedications)
+	// medicationGroup.Get("list", medication.HandleListMedications)
+	// medicationGroup.Post("add", medication.HandleAddMedication)
+	// medicationGroup.Put("update/:id", medication.HandleUpdateMedication)
+	// medicationGroup.Delete("delete/:id", medication.HandleDeleteMedication)
 }
